132c936b14b459f997edd8357fc0788d1b1ec0e3
Anna Bernas <bernas.anna@icloud.com>
2019-02-06 22:59:49 +0100

    created first structure

---
+++ inc/employee.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+class Employee
+{
+    int _id;
+    std::string _name;
+    std::strng _surmname;
+    bool _cashier;
+    bool _service;
+    bool _cook;
+    bool _manager;
+    bool _sink;
+};
